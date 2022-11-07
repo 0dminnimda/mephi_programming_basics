c68c50c292a62e97052755ef21e4ed9db122d7dc
0dminnimda <0dminnimda@gmail.com>
2022-11-07 20:32:18 +0300

    Create lab3\commands.c

---
+++ lab3/commands.c
@@ -0,0 +1,13 @@
+#include "commands.h"
+
+#include <stdio.h>
+
+void init(char *s) {}
+
+void insert(char *s) {}
+
+void remove_at(char *s) {}
+
+void process_data(char *s) {}
+
+void print(char *s) {}
