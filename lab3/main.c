2ab6f1e0204e0399046ff5ed5f1f42fbc1912192
0dminnimda <0dminnimda@gmail.com>
2022-11-08 13:02:33 +0300

    lab3/main.c: temporary implementation for all of the convinience features

--- lab3/main.c
+++ lab3/main.c
@@ -6,33 +6,29 @@
 
 #define COMMAND_COUNT 5
 
-void greet() {}
-void print_commands(const char *command_names[], size_t count) {}
-void incorrect_input(const char *command_names[], size_t count) {}
+void greet() {
+    printf("Hello world!\n");
+}
+
+void print_commands(const char *command_names[], size_t count) {
+    printf("Available commands:\n");
+    for (size_t i = 0; i < count; i++) {
+        printf("- '%s'\n", command_names[i]);
+    }
+}
+
+void incorrect_input(const char *command_names[], size_t count) {
+    printf("Wrong input, try again!\n");
+    print_commands(command_names, count);
+}
 
 void partition(char *string, char **first, char **second) {
-    *first = strtok(string, " ");
-    *second = strtok(NULL, "");
-    return;
-    
-    int second_start = -1;
-    int state = 0;
-    int i = 0;
-    while (string[i] != '\0') {
-        if (state == 0 && string[i] == ' ') {
-            string[i] = '\0';
-            printf("%d\n", string[i] == '\0');
-            second_start = i;
-            state = 1;
-        } else if (state == 1 && string[i] != ' ') {
-            second_start = i;
-            break;
-        }
-       // putc(string[i], stdout);
-        i++;
-    }
-    *first = string;
-    *second = (string + second_start);
+    *first = strtok(string, " \n");
+    if (*first == NULL)
+        *first = "";
+    *second = strtok(NULL, "\n");
+    if (*second == NULL)
+        *second = "";
 }
 
 char *read() {
@@ -45,7 +41,7 @@ char *read() {
 ssize_t match_command(const char *command_names[], size_t count,
                   const char *command_name) {
     for (size_t i = 0; i < count; i++)
-        if (command_name == command_names[i]) return i;
+        if (strcmp(command_name, command_names[i]) == 0) return i;
 
     return -1;
 }
@@ -58,35 +54,17 @@ int main() {
         "init", "insert", "remove_at", "process_data", "print",
     };
 
-    char *first, *second;
-    char ss[] = "1234  5678 9012";
-    /*printf("gg\n");
-    first = strtok(ss, " ");
-    printf("hh\n");
-    second = strtok(NULL, "");
-    printf("ss\n");*/
-    partition(ss, &first, &second);
-    //ss[4] = '\0';
-    /*for (int i = 0; i < 16; i ++) {
-        if (first[i] == '\0')
-            putc('|', stdout);
-        else
-            putc(first[i], stdout);
-    }*/
-    printf("'%s' '%s'\n", first, second);
-    return 0;
-
     char *exit_command_name = "exit";
 
     greet();
+    print_commands(command_names, COMMAND_COUNT);
 
     while (1) {
-        print_commands(command_names, COMMAND_COUNT);
-
         char *command_name, *command_input;
         partition(read(), &command_name, &command_input);
+        printf("'%s' '%s'\n", command_name, command_input);
 
-        if (command_name == exit_command_name) break;
+        if (strcmp(command_name, exit_command_name) == 0) break;
 
         ssize_t command_index =
             match_command(command_names, COMMAND_COUNT, command_name);
@@ -94,6 +72,6 @@ int main() {
         if (command_index == -1)
             incorrect_input(command_names, COMMAND_COUNT);
         else
-            commands[(size_t)command_index](command_input);
+            commands[command_index](command_input);
     }
 }
