9d7ae932b35e78de49d944c9110be4d78d5337fc
0dminnimda <0dminnimda@gmail.com>
2022-11-09 20:57:28 +0300

    lab3\commands.c: fix and clean up init_command

--- lab3/commands.c
+++ lab3/commands.c
@@ -4,55 +4,17 @@
 #include <string.h>
 
 int init_command(char *str, Vector vec) {
-    size_t str_len = strlen(str);
-
+    reset_length(vec);
+    float value;
     int offset;
     char *ptr = str;
-
-    size_t len;
-    printf("%p %p %d | ", str, ptr, str_len);
-    if (sscanf_s(ptr, "%zu%n", &len, &offset, str_len) != 1) {
-        printf("Invalid input, expected an integer\n");
-        return 0;
-    }
-    ptr += offset;
-    str_len -= offset;
-
-    if (reserve(vec, len)) {
-        printf("Could not reserve %zu items for Vector\n", len);
-        return 0;
-    }
-
-    for (size_t i = 0; i < len; i++) {
-        float value;
-        printf("%p %p %d | ", str, ptr, str_len);
-        if (sscanf_s(ptr, "%f%n", &value, &offset, str_len) != 1) {
-            printf("Invalid input, expected an integer\n");
+    while (sscanf_s(ptr, "%f%n", &value, &offset) == 1) {
+        ptr += offset;
+        if (push_back(vec, value)) {
+            printf("Could not push_back %f into Vector\n", value);
             return 0;
         }
-        ptr += offset;
-        str_len -= offset;
-        set_item(vec, i, value);
-        // if (push_back(vec, value)) {
-        //     printf("Could not push_back %f into Vector\n", value);
-        //     return 0;
-        // }
     }
-    printf("%p %p %d | ", str, ptr, str_len);
-
-    // float value;
-    // int offset;
-    // char *ptr = str;
-    // while (printf(">'%s'< ", ptr), sscanf(ptr, "%f%n", &value, &offset) == 1) {
-    //     printf("%f ", value);
-    //     if (push_back(vec, value)) {
-    //         printf("Could not push_back %f into Vector\n", value);
-    //         return 0;
-    //     }
-    //     ptr += offset;
-    // }
-
-    printf("end\n");
     return 0;
 }
 
