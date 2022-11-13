8795d2c34779b20f914faaf539575f84508388e6
0dminnimda <0dminnimda@gmail.com>
2022-11-13 20:49:30 +0300

    lab3\rational.c: remove useless comments

--- lab3/rational.c
+++ lab3/rational.c
@@ -6,8 +6,6 @@
 typedef struct RationalNode_ {
     long long decimal;
     unsigned long long fractional;
-    // char *decimal;
-    // char *fractional;
 } RationalNode_;
 
 int sscan_rational(char *str_ptr, Rational *rat, int *offset) {
@@ -33,34 +31,8 @@ int sscan_rational(char *str_ptr, Rational *rat, int *offset) {
 
     return 0;
 }
-/*
 
-
-    printf("Created!\n");
-
-    int offset = 0;
-    if (sscanf_s(str_ptr, "%18lld%n", &(*rat)->decimal, &offset) != 1)
-        return -1;
-    str_ptr += offset;
-    printf("First %lld! Chrs: %d! Rest: %s!\n", (*rat)->decimal, offset1, str_ptr);
-
-    if (str_ptr + offset1 == '.') {
-        offset1 += 1;
-        int res = sscanf_s(str_ptr + offset1, "%llu%n", &(*rat)->fractional, &offset2);
-        printf("Second %llu! Chrs: %d! Res %d\n", (*rat)->fractional, offset2, res);
-        if (res == 0 || res == 1) return -1;
-        offset2 * res
-    } else printf("No second");
-
-    printf("Chrs: %d\n", offset1 + offset2 * res);
-    *offset = offset1 + offset2 * res;
-
-*/
-void destroy_rational(Rational rat) {
-    // free(rat->decimal);
-    // free(rat->fractional);
-    free(rat);
-}
+void destroy_rational(Rational rat) { free(rat); }
 
 void print_rational(Rational rat) {
     printf("%lld.%llu", rat->decimal, rat->fractional);
