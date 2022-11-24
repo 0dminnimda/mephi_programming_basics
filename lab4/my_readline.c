5c893e0f5e0db03226298f7562758ac4261f3b1c
0dminnimda <0dminnimda@gmail.com>
2022-11-24 18:35:06 +0300

    lab4\my_readline.c: format and remove useless prints

--- lab4/my_readline.c
+++ lab4/my_readline.c
@@ -5,19 +5,17 @@
 #define CHUNK_SIZE 2
 #define CHUNK_SIZED_STRING_FORMAT "%2[^\n]"
 
-char *my_readline(char *prompt) {
-    if (prompt)
-        printf("%s", prompt);
+char *my_readline(const char *prompt) {
+    if (prompt) printf("%s", prompt);
 
     char *buf = NULL;
     char *end = NULL;
 
     int scanf_result = 0;
     int end_reached = 0;
-    while (!end_reached) {        
+    while (!end_reached) {
         // extend the memory by one chunk
-        printf("New length %d, new size %d\n", end - buf + CHUNK_SIZE, (end - buf + CHUNK_SIZE) * sizeof(char));
-        char *new_buf = (char*)realloc(buf, (end - buf + CHUNK_SIZE) * sizeof(char));
+        char *new_buf = realloc(buf, (end - buf + CHUNK_SIZE) * sizeof(char));
         if (new_buf == NULL) {
             free(buf);
             return NULL;
@@ -27,32 +25,24 @@ char *my_readline(char *prompt) {
 
         // read new chunk
         int scanf_result = scanf(CHUNK_SIZED_STRING_FORMAT, end);
-        printf("Read '%s'\n", end);
 
-        printf("Scanf result %d\n", scanf_result);
-
-        // check if we hit the end on the line
+        // check if we hit the end of the line
         char *pos;
         for (pos = end; pos < end + CHUNK_SIZE; pos++) {
-            printf("Char checked '%c'\n", *pos);
             if (*pos == '\0' || *pos == '\n') {
                 end_reached = 1;
                 break;
             }
         }
         end = pos;
-        printf("New start %p, end %p, len %d\n", buf, end, end - buf);
     }
 
-    if ((end - buf) || (scanf_result != EOF))
-        return buf;
-    printf("Null the result\n");
+    if ((end - buf) || (scanf_result != EOF)) return buf;
     free(buf);
     return NULL;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     char *line = my_readline(0);
     printf("Returned ");
     printf("'%s'\n", line);
