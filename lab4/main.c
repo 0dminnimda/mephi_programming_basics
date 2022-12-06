fe3cd2027a9b4af2c58d059c295a270d025c6e01
0dminnimda <0dminnimda@gmail.com>
2022-12-06 18:27:16 +0300

    lab4/main.c: try to implement word alphabets

--- lab4/main.c
+++ lab4/main.c
@@ -24,6 +24,14 @@ Vector split(char *line) {
     return words;
 }
 
+#define BITS_IN_LL 64
+#define FIRST_CHAR 0
+
+typedef struct alphabet_t {
+    long long lo;
+    long long hi;
+} alphabet_t;
+
 int main(int argc, char *argv[]) {
     printf("Input words\n");
     char *line = my_readline(NULL);
@@ -45,6 +53,68 @@ int main(int argc, char *argv[]) {
     }
     printf("\n");
 
+    alphabet_t *alphabets = malloc(get_length(words) * sizeof(alphabet_t));
+    if (get_length(words) && alphabets == NULL) {
+        printf("ERROR: could not allocate new memory\n");
+        return 0;
+    }
+
+    printf("Words use those alphabets:\n");
+    for (size_t i = 0; i < get_length(words); i++) {
+        alphabets[i].lo = 0;
+        alphabets[i].hi = 0;
+        char *word = get_item(words, i);
+        printf("%s ---- ", word);
+        while (*word != '\0') {
+            char coded = *word - FIRST_CHAR;
+            printf("%d %c %d %d\n", (int)*word, *word, coded < BITS_IN_LL, (char)(coded - BITS_IN_LL*(coded >= BITS_IN_LL)));
+            if (coded < BITS_IN_LL)
+                alphabets[i].lo |= (1 << coded);
+            else
+                alphabets[i].hi |= (1 << (coded - BITS_IN_LL));
+            word++;
+            // for ()
+            // printf();
+        }
+
+#if 1
+
+        // debug output
+        printf("\n");
+        for (size_t j = '!'; j < 2*BITS_IN_LL; j++) {
+            printf("%c", (char)(j + FIRST_CHAR));
+        }
+        printf("\n");
+        for (size_t j = '!'; j < BITS_IN_LL; j++) {
+            printf("%d", (alphabets[i].lo & (1 << j)) != 0);
+        }
+        for (size_t j = 0; j < BITS_IN_LL; j++) {
+            printf("%d", (alphabets[i].hi & (1 << j)) != 0);
+        }
+        printf("\n");
+
+#else
+
+#endif
+
+        printf("%d %d :\n", alphabets[i].lo, alphabets[i].hi);
+        for (long long j = 0; j < BITS_IN_LL; j++) {
+            char decoded = j + FIRST_CHAR;
+            if (alphabets[i].lo & (1 << j))
+                printf("%d %c %d\n", (int)decoded, decoded, (char)j);
+                // printf("%c", decoded);
+        }
+        printf("s\n");
+        for (long long j = 0; j < BITS_IN_LL; j++) {
+            char decoded = j + FIRST_CHAR + BITS_IN_LL;
+            if (alphabets[i].hi & (1 << j))
+                printf("%d %c %d\n", (int)decoded, decoded, (char)j);
+                // printf("%c", decoded);
+        }
+        printf("\n");
+    }
+
+    free(alphabets);
     free(words);
     free(line);
     return 0;
