15087798e07ae3fd431808983c6f8b58275b5d27
0dminnimda <0dminnimda@gmail.com>
2022-12-07 12:28:35 +0300

    lab4\main.c: clean up, make prettier

--- lab4/main.c
+++ lab4/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "my_readline.h"
 #include "my_string.h"
 
-#define VECTOR_ITEM char*
+#define VECTOR_ITEM char *
 #define VECTOR_ITEM_DESTRUCTOR(item) free(item)
 
 #include "vector.h"
@@ -25,7 +26,6 @@ Vector split(char *line) {
 }
 
 #define BITS_IN_LL 64
-#define FIRST_CHAR 0
 
 typedef unsigned long long alphabet_sub_t;
 
@@ -33,6 +33,8 @@ typedef struct alphabet_t {
     alphabet_sub_t lo;
     alphabet_sub_t hi;
 } alphabet_t;
+
+#define MASK(shift) ((alphabet_sub_t)1 << (shift))
 
 int main(int argc, char *argv[]) {
     printf("Input words\n");
@@ -51,7 +53,7 @@ int main(int argc, char *argv[]) {
 
     printf("The words I found:\n");
     for (size_t i = 0; i < get_length(words); i++) {
-        printf("%c'%s'", ' '*(i != 0), get_item(words, i));
+        printf("%c'%s'", ' ' * (i != 0), get_item(words, i));
     }
     printf("\n");
 
@@ -65,53 +67,23 @@ int main(int argc, char *argv[]) {
     for (size_t i = 0; i < get_length(words); i++) {
         alphabets[i].lo = 0;
         alphabets[i].hi = 0;
+
         char *word = get_item(words, i);
-        printf("%s ---- ", word);
+        printf("'%s' uses: '", word);
+
         while (*word != '\0') {
-            char coded = *word - FIRST_CHAR;
-            printf("%d %c %d %d\n", (int)*word, *word, coded < BITS_IN_LL, (char)(coded - BITS_IN_LL*(coded >= BITS_IN_LL)));
-            if (coded < BITS_IN_LL)
-                alphabets[i].lo |= ((alphabet_sub_t)1 << coded);
+            if (*word < BITS_IN_LL)
+                alphabets[i].lo |= MASK(*word);
             else
-                alphabets[i].hi |= ((alphabet_sub_t)1 << (coded - BITS_IN_LL));
+                alphabets[i].hi |= MASK(*word - BITS_IN_LL);
             word++;
         }
 
-#if 1
-
-        // debug output
-        printf("\n");
-        for (size_t j = '!'; j < 2*BITS_IN_LL; j++) {
-            printf("%c", (char)(j + FIRST_CHAR));
-        }
-        printf("\n");
-        for (size_t j = '!'; j < BITS_IN_LL; j++) {
-            printf("%d", (alphabets[i].lo & ((alphabet_sub_t)1 << j)) != 0);
-        }
-        for (size_t j = 0; j < BITS_IN_LL; j++) {
-            printf("%d", (alphabets[i].hi & ((alphabet_sub_t)1 << j)) != 0);
-        }
-        printf("\n");
-
-#else
-
-#endif
-
-        printf("%llu %llu :\n", alphabets[i].lo, alphabets[i].hi);
-        for (size_t j = 0; j < BITS_IN_LL; j++) {
-            char decoded = j + FIRST_CHAR;
-            if (alphabets[i].lo & ((alphabet_sub_t)1 << j))
-                printf("%d %c %d\n", (int)decoded, decoded, (char)j);
-                // printf("%c", decoded);
-        }
-        printf("s\n");
-        for (size_t j = 0; j < BITS_IN_LL; j++) {
-            char decoded = j + FIRST_CHAR + BITS_IN_LL;
-            if (alphabets[i].hi & ((alphabet_sub_t)1 << j))
-                printf("%d %c %d\n", (int)decoded, decoded, (char)j);
-                // printf("%c", decoded);
-        }
-        printf("\n");
+        for (char j = 0; j < BITS_IN_LL; j++)
+            if (alphabets[i].lo & MASK(j)) printf("%c", j);
+        for (char j = 0; j < BITS_IN_LL; j++)
+            if (alphabets[i].hi & MASK(j)) printf("%c", j + BITS_IN_LL);
+        printf("' (%llu %llu)\n", alphabets[i].lo, alphabets[i].hi);
     }
 
     free(alphabets);
