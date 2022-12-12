52aec3ece2f99eb76730625d2a49c31c07db32f9
0dminnimda <0dminnimda@gmail.com>
2022-12-12 22:21:41 +0300

    lab4/words.c: print words in parentecies

--- lab4/words.c
+++ lab4/words.c
@@ -18,9 +18,10 @@ Words split(char *line) {
 
 void print_words(Words words) {
     size_t shown = 0;
+    printf("\"");
     for (size_t i = 0; i < get_length(words); i++) {
         if (*get_item(words, i))
             printf("%c%s", ' ' * (shown++ != 0), get_item(words, i));
     }
-    printf("\n");
+    printf("\"\n");
 }
