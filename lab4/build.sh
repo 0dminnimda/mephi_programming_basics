548c72f9aaa50d9e0e375060b1d49df6e4de4c28
0dminnimda <0dminnimda@gmail.com>
2022-12-13 01:59:12 +0300

    lab4\build.sh: add ability to choose program id

--- lab4/build.sh
+++ lab4/build.sh
@@ -5,9 +5,13 @@ EXTRA_ARGS=""
 
 which cc &> /dev/null
 
-if [[ $? != "0" ]]; then 
+if [[ $? != "0" ]]; then
     MY_CC="clang"
-    EXTRA_ARGS="-Wno-deprecated-declarations"
+    EXTRA_ARGS="$EXTRA_ARGS -Wno-deprecated-declarations"
+fi
+
+if [[ $1 == "1" ]]; then
+    EXTRA_ARGS="$EXTRA_ARGS -lreadline -D PROGRAM_ID=1"
 fi
 
 $MY_CC words.c my_string.c my_readline.c main.c -o main.o -O3 -g $EXTRA_ARGS
