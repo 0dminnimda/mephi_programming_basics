5a1e03585456b20f7d1a3693b17ff7a302e74e8f
0dminnimda <0dminnimda@gmail.com>
2022-12-15 02:54:17 +0300

    Update lab4\my_readline copy.c - attempt to fix readline

--- lab4/my_readline copy.c
+++ lab4/my_readline copy.c
@@ -6,18 +6,31 @@
 #define CHUNK_SIZE 2
 #define CHUNK_SIZED_STRING_FORMAT "%2[^\n]%n"
 
+static int scan_any(char *str, int *offset) {
+    int scanf_result;
+
+    scanf_result = scanf("%1[\n]%n", str, offset);
+    if (scanf_result == 1) return scanf_result;
+
+    scanf_result = scanf("%1[^\n]%n", str, offset);
+    if (scanf_result == 1) return scanf_result;
+
+    *offset = 0;
+    return scanf_result;
+}
+
 char *readline(const char *prompt) {
     if (prompt) printf("%s", prompt);
 
-    // read at most one symbol + null terminator to check our current state 
+    // read at most one symbol + null terminator to check our current state
     // is it eof, \n, normal input or nothing
     char *buf = malloc(2 * sizeof(char));
     if (buf == NULL) return NULL;
     char *end = buf;
 
     int offset = 0;
-    int scanf_result = scanf("%1s%n", end, &offset);
-    printf("  %d\n", offset);
+    int scanf_result = scan_any(end, &offset);
+    printf("  %d %d - %c %d\n", scanf_result, offset, *end, (int)*end);
 
     offset = offset != 0;
     if (*end == '\n') offset = 0;
@@ -37,6 +50,11 @@ char *readline(const char *prompt) {
         // read new chunk
         offset = 0;
         scanf_result = scanf(CHUNK_SIZED_STRING_FORMAT, end, &offset);
+    }
+
+    if (*end != '\n') {
+        char junk[2];
+        scanf("%1[\n]", junk);
     }
 
     if ((end - buf == 0) && (scanf_result == EOF)) {
