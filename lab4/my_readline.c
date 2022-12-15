f05cd9a35b0252ced3954258ba5570e5724abdc1
0dminnimda <0dminnimda@gmail.com>
2022-12-15 03:24:52 +0300

    lab4\my_readline.c: hopefully fix some of the problems

--- lab4/my_readline.c
+++ lab4/my_readline.c
@@ -14,30 +14,31 @@ char *readline(const char *prompt) {
 
     // read at most one symbol to set offset and result - representing state
     // current state can be eof, \n, normal input or nothing
-    int offset = 0;
     int scanf_result = scanf("%1[\n]", end);
-    if (scanf_result != 1) scanf_result = scanf("%1[^\n]%n", end, &offset);
 
-    // check if we hit the end of the line
-    while (scanf_result == 1 && offset != 0) {
-        end += offset;
+    if (scanf_result != 1) {
+        int offset = 0;
+        scanf_result = scanf("%1[^\n]%n", end, &offset);
 
-        // extend the memory by one chunk
-        char *new_buf = realloc(buf, (end - buf + CHUNK_SIZE) * sizeof(char));
-        if (new_buf == NULL) {
-            free(buf);
-            return NULL;
+        // check if we hit the end of the line
+        while (scanf_result == 1 && offset != 0) {
+            end += offset;
+
+            // extend the memory by one chunk
+            char *new_buf = realloc(buf, (end - buf + CHUNK_SIZE) * sizeof(char));
+            if (new_buf == NULL) {
+                free(buf);
+                return NULL;
+            }
+            end = new_buf + (end - buf);
+            buf = new_buf;
+
+            // read new chunk
+            offset = 0;
+            scanf_result = scanf(CHUNK_SIZED_STRING_FORMAT, end, &offset);
         }
-        end = new_buf + (end - buf);
-        buf = new_buf;
 
-        // read new chunk
-        offset = 0;
-        scanf_result = scanf(CHUNK_SIZED_STRING_FORMAT, end, &offset);
-    }
-
-    // consume the potential \n if we have read at least something non-\n
-    if (end && *end != '\n') {
+        // consume the potential \n
         char junk[2];
         scanf("%1[\n]", junk);
     }
