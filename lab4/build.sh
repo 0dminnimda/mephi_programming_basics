228ce57c79d5204051af61e7f802c864e4512165
0dminnimda <0dminnimda@gmail.com>
2022-12-16 20:47:47 +0300

    lab4\build.sh: use ARGS for all arguments

--- lab4/build.sh
+++ lab4/build.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 MY_CC="cc"
-EXTRA_ARGS=""
+ARGS="alphabet.c words.c main.c -o main.o -O3 -g"
 
 which cc &> /dev/null
 
 if [[ $? != "0" ]]; then
     echo "I did not find 'cc', so using 'clang' instead"
     MY_CC="clang"
-    EXTRA_ARGS="$EXTRA_ARGS -Wno-deprecated-declarations"
+    ARGS="$ARGS -Wno-deprecated-declarations"
 fi
 
 if [[ $1 == "1" ]]; then
-    EXTRA_ARGS="$EXTRA_ARGS -lreadline -D PROGRAM_ID=1"
+    ARGS="$ARGS -lreadline -D PROGRAM_ID=1"
 else
-    EXTRA_ARGS="$EXTRA_ARGS my_string.c my_readline.c"
+    ARGS="my_string.c my_readline.c $ARGS"
 fi
 
-COMMAND="$MY_CC alphabet.c words.c main.c -o main.o -O3 -g $EXTRA_ARGS"
+COMMAND="$MY_CC $ARGS"
 
 printf "Running '$COMMAND'\n\n"
 
