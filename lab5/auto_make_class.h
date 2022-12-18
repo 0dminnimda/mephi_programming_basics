e189ea1723248148a6dccc538d18d5f449fea792
0dminnimda <0dminnimda@gmail.com>
2022-12-18 05:07:13 +0300

    L5L auto_make_class.h: try to make the thing better, but break it

--- lab5/auto_make_class.h
+++ lab5/auto_make_class.h
@@ -2,18 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define OFFSETOF(type, field) ((size_t)&(((type *)0)->field))
+#define FIELD_AT(obj, index) (obj + obj->type->offsets[index])
+#define FUNCS_AT(obj, index) (obj->type->functions[index])
+
+typedef struct Object Object;
+
+typedef struct FuncTable {
+    int (*greater)(Object *l, Object *r);
+    void (*fprint)(FILE *const stream, Object *it);
+} FuncTable;
+
+typedef struct Type {
+    char **names;
+    size_t *offsets;
+    FuncTable *functions;
+    size_t field_count;
+} Type;
+
+#define OBJECT_FIELDS Type *type;
+typedef struct Object { OBJECT_FIELDS; } Object;
+
+void fpint(FILE *const stream, Object *obj, size_t index) {
+    FUNCS_AT(obj, index).fprint(stream, FIELD_AT(obj, index));
+}
+
+int greater(Object *lhs, Object *rhs, size_t index) {
+    if (lhs->type != rhs->type) return 0;
+    FUNCS_AT(lhs, index).greater(FIELD_AT(lhs, index), FIELD_AT(rhs, index));
+}
+
+size_t offset_of(Object *obj, char *name) {
+    for (size_t result = 0; result < obj->type->field_count; result++) {
+        if (strcmp(obj->type->names[result], name) == 0) {
+            return result;
+        }
+    }
+    return -1;
+}
+
+
 #define CLASS_NAME Data
 #define FIELDS FIELD(int, a, l > r, "%d") FIELD(char*, b, strcmp(l, r) > 0, "%s") FIELD(long, c, l > r, "%ld")
 
-typedef struct FuncTable {
-    int (*greater)(void *l, void *r);
-    void (*fprint)(FILE *const stream, void *it);
-} FuncTable;
-
-typedef struct Type { char **names; size_t *offsets; FuncTable *functions; size_t field_count; } Type;
-
 #define FIELD(type, name, greater_expr, format) type name;
-typedef struct CLASS_NAME { Type *type; FIELDS; } CLASS_NAME;
+typedef struct CLASS_NAME { OBJECT_FIELDS; FIELDS; } CLASS_NAME;
 #undef FIELD
 
 #define FIELD(type, name, greater_expr, format) \
@@ -27,8 +60,6 @@ static void impl_##CLASS_NAME##_##name##_fprint(FILE *const stream, void *ptr) {
 }
 FIELDS
 #undef FIELD
-
-#define OFFSETOF(TYPE, ELEMENT) ((size_t)&(((TYPE *)0)->ELEMENT)) 
 
 static inline Type make_type() {
     size_t count = 0;
@@ -69,61 +100,37 @@ static inline Type make_type() {
 
 #define TYPE_MAKER Type make_##CLASS_NAME##_type() { return make_type(); }
 TYPE_MAKER
-
-#define MAKE_INSTANCE(type, ...) {__VA_ARGS__, type}
-
-// CLASS_NAME make_##CLASS_NAME() {
-// #define FIELD(type, name, greater_expr, format) sizeof(type),
-//     return (CLASS_NAME){FIELDS};
-// #undef FIELD
-// }
+#undef TYPE_MAKER
 
 #undef FIELDS
 
-#define TYPE(instance) (*(Type **)instance)
-
-void fpint(FILE *const stream, void *instance, size_t index) {
-    Type *tp = TYPE(instance);
-    tp->functions[index].fprint(stream, instance + tp->offsets[index]);
-}
-
-int greater(void *instance_l, void *instance_r, size_t index) {
-    Type *tp = TYPE(instance_l);
-    if (tp != TYPE(instance_r)) return 0;
-    tp->functions[index].greater(instance_l + tp->offsets[index], instance_r + tp->offsets[index]);
-}
-
-size_t offset_of(void *instance, char *name) {
-    Type *tp = TYPE(instance);
-
-    for (size_t result = 0; result < tp->field_count; result++) {
-        if (strcmp(tp->names[result], name) == 0) {
-            return result;
-        }
-    }
-    return -1;
-}
-
 int main()
 {
+    //printf("%s\n", STR(impl_##CLASS_NAME##_##name##_greater));
     Type DataType = make_type();
-    Data data1 = {&DataType, 4, "dsdfgdf", 0};
-    Data data2 = {&DataType, 3, "", 123};
+    // char **name = {"a", "b", "c"};
+    // size_t *offsets = {OFFSETOF(CLASS_NAME, a), OFFSETOF(CLASS_NAME, b), OFFSETOF(CLASS_NAME, c)};
+    // FuncTable *funcs = {
+    //     (FuncTable){impl_Data_a_greater, impl_Data_a_fprint},
+    //     (FuncTable){impl_Data_b_greater, impl_Data_b_fprint},
+    //     (FuncTable){impl_Data_c_greater, impl_Data_c_fprint},
+    // };
+    // Type DataType = {
+    //     name,
+    //     offsets,
+    //     funcs,
+    //     3
+    // };
+    Data o_data1 = {&DataType, 4, "dsdfgdf", 0};
+    Data o_data2 = {&DataType, 3, "", 123};
+    Object *data1 = (Object *)&o_data1;
+    Object *data2 = (Object *)&o_data2;
 
-    // printf("%p %zu\n", &data1.type, sizeof(Type*));
-    // printf("%p %zu\n", &data1.a, sizeof(int));
-    // printf("%p %zu\n", &data1.b, sizeof(char*));
-    // printf("%p %zu\n", &data1.c, sizeof(long));
-    // printf("%p\n", DataType.functions[0].fprint);
-    // printf("%p\n", DataType.functions[1].fprint);
-    // printf("%p\n", DataType.functions[2].fprint);
-    // DataType.functions[2].fprint(stdout, &data1.c);
+    printf("a -> %zu, b -> %zu, c -> %zu\n", offset_of(data1, "a"), offset_of(data1, "b"), offset_of(data1, "c"));
 
-    printf("a -> %zu, b -> %zu, c -> %zu\n", offset_of(&data1, "a"), offset_of(&data1, "b"), offset_of(&data1, "c"));
-
-    fpint(stdout, &data1, 0); printf(" > "); fpint(stdout, &data2, 0); printf(" = %d\n", greater(&data1, &data2, 0)); 
-    printf("'"); fpint(stdout, &data1, 1); printf("' > '"); fpint(stdout, &data2, 1); printf("' = %d\n", greater(&data1, &data2, 1)); 
-    fpint(stdout, &data1, 2); printf(" > "); fpint(stdout, &data2, 2); printf(" = %d\n", greater(&data1, &data2, 2)); 
+    fpint(stdout, data1, 0); printf(" > "); fpint(stdout, data2, 0); printf(" = %d\n", greater(data1, data2, 0)); 
+    printf("'"); fpint(stdout, data1, 1); printf("' > '"); fpint(stdout, data2, 1); printf("' = %d\n", greater(data1, data2, 1)); 
+    fpint(stdout, data1, 2); printf(" > "); fpint(stdout, data2, 2); printf(" = %d\n", greater(data1, data2, 2)); 
 
 
     return 0;
