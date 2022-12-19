4bc70ae7a093f7b8e82884a620d35e90de04184a
0dminnimda <0dminnimda@gmail.com>
2022-12-19 21:51:16 +0300

    Create lab5\voters.c and lab5\voters.h

---
+++ lab5/voters.c
@@ -0,0 +1,45 @@
+#include "voters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STR(x) #x
+
+voter_field str2field(char *str) {
+    if (strcmp(str, STR(voter_full_name) + 2) == 0) return voter_full_name;
+    if (strcmp(str, STR(voter_station_id) + 2) == 0) return voter_station_id;
+    if (strcmp(str, STR(voter_age) + 2) == 0) return voter_age;
+    return voter_field_count;
+}
+
+#define VOTER_FORMAT "%s, %7s, %d"
+
+void fprint_voter(FILE *const stream, Voter voter) {
+    fprint(stream, VOTER_FORMAT, voter.full_name, voter.station_id, voter.age);
+}
+
+int fscanf_voter(FILE *const stream, Voter *voter) {
+    return fscanf(stream, VOTER_FORMAT, &voter->full_name, &voter->station_id,
+                  &voter->age);
+}
+
+#define STRUCT_CMP(structure, name) structure##_##name##_cmp
+
+#define MAKE_STRUCT_CMP(structure, type, name, greater_expr)    \
+    static int STRUCT_CMP(structure, name)(structure * l_ptr,   \
+                                           structure * r_ptr) { \
+        type l = l_ptr->name;                                   \
+        type r = r_ptr->name;                                   \
+        return greater_expr;                                    \
+    }
+
+MAKE_STRUCT_CMP(Voter, char *, full_name, strcmp(l, r) > 0)
+MAKE_STRUCT_CMP(Voter, station_t, station_id, strcmp(l, r) > 0)
+MAKE_STRUCT_CMP(Voter, int, age, l > r)
+
+cmp_func_t *field2cmp(voter_field field) {
+    if (field == voter_full_name) return STRUCT_CMP(Voter, full_name);
+    if (field == voter_station_id) return STRUCT_CMP(Voter, station_id);
+    return STRUCT_CMP(Voter, age);
+}
---
+++ lab5/voters.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+typedef char station_t[7];
+typedef struct {
+    char *full_name;
+    station_t station_id;
+    int age;
+} Voter;
+
+void fprint_voter(FILE *const stream, Voter voter);
+int fscanf_voter(FILE *const stream, Voter *voter);
+
+typedef enum {
+    voter_full_name,
+    voter_station_id,
+    voter_age,
+    voter_field_count
+} voter_field;
+
+voter_field str2field(char *str);
+
+typedef int cmp_func_t(Voter *, Voter *);
+cmp_func_t *field2cmp(voter_field field);
+
+#define VECTOR_ITEM Voter
+#define VECTOR_ITEM_DESTRUCTOR(item) free(item.full_name)
+#include "vector.h"
+
+typedef Vector Voters;
