109c15ecf95441ca358efc2ed38093b7849dbec3
0dminnimda <0dminnimda@gmail.com>
2022-12-21 09:10:25 +0300

    lab5\main.c: add vector errors and handle parse in if-stmt

--- lab5/main.c
+++ lab5/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define VECTOR_PRINT_ERROR(message) printf("ERROR: %s\n", message)
 #include "argparse.h"
 
 
 int main(int argc, char *argv[]) {
     Options options;
-    return parse(argc, argv, &options);
+    if (parse(argc, argv, &options)) return -1;
+
 }
