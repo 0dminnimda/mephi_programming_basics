af5ed376a574f534579aa5a1ce9d8cd191ffdf61
0dminnimda <0dminnimda@gmail.com>
2022-12-21 10:42:03 +0300

    lab5\main.c: add read_file_data() and get_data()

--- lab5/main.c
+++ lab5/main.c
@@ -6,6 +6,22 @@
 #define VECTOR_PRINT_ERROR(message) printf("ERROR: %s\n", message)
 #include "argparse.h"
 
+Voters read_file_data(Options options) {
+    Voters voters;
+    if (create_vector(&voters, 0)) return NULL;
+
+    FILE *input_file;
+    input_file = fopen(options.input_file, "r");
+
+    if (input_file == NULL) return NULL;
+
+    Voter voter;
+    while (fscanf_voter(input_file, &voter) == 0) vec_push_back(voters, voter);
+
+    fclose(input_file);
+    return voters;
+}
+
 Voters generate_data(Options options) {
     Voters voters;
     if (create_vector(&voters, 0)) return NULL;
@@ -19,6 +35,10 @@ Voters generate_data(Options options) {
     return voters;
 }
 
+Voters get_data(Options options) {
+    if (options.input_file) return read_file_data(options);
+    return generate_data(options);
+}
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
