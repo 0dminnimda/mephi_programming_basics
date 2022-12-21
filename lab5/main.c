1772a48d6df1bc082976470f7c144234c600d87d
0dminnimda <0dminnimda@gmail.com>
2022-12-21 10:41:47 +0300

    lab5\main.c: add generate_data() and init random in main

--- lab5/main.c
+++ lab5/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define VECTOR_PRINT_ERROR(message) printf("ERROR: %s\n", message)
 #include "argparse.h"
 
+Voters generate_data(Options options) {
+    Voters voters;
+    if (create_vector(&voters, 0)) return NULL;
+
+    Voter voter;
+    for (size_t i = 0; i < options.array_length; i++) {
+        make_fake_voter(&voter);
+        vec_push_back(voters, voter);
+    }
+
+    return voters;
+}
+
 
 int main(int argc, char *argv[]) {
+    srand(time(NULL));
+
     Options options;
     if (parse(argc, argv, &options)) return -1;
 
