9545dd231e5762211765e5add539e37273b37e62
0dminnimda <0dminnimda@gmail.com>
2022-12-27 19:33:57 +0300

    lab4\my_string: implement strlen()

--- lab4/my_string.c
+++ lab4/my_string.c
@@ -1,5 +1,11 @@
 #include "my_string.h"
 
+size_t strlen(const char *str) {
+    size_t len = -1;
+    while (str[++len] != '\0');
+    return len;
+}
+
 size_t strcspn(const char *str1, const char *str2) {
     size_t index = -1;
     while (str1[++index] != '\0') {
--- lab4/my_string.h
+++ lab4/my_string.h
@@ -1,3 +1,4 @@
 #include <stddef.h>
 
+size_t strlen(const char *str);
 size_t strcspn(const char *str1, const char *str2);
