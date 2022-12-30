344c2c82c3881cbc273cd2bd21690d012ce0781c
0dminnimda <0dminnimda@gmail.com>
2022-12-30 16:05:45 +0300

    lab6\main.c: add read_line() & remove_excess_spaces()

--- lab6/main.c
+++ lab6/main.c
@@ -1,4 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "arena.c"
+#include "char_list.h"
+
+#define MAX_LINE_LENGTH ARENA_SIZE / 2
+
+void read_line(CharNodeArena *arena, CharList *list) {
+    CharNode *tail = NULL;
+    list->head = NULL;
+
+    for (size_t i = 0; i < MAX_LINE_LENGTH; i++) {
+        char value = getchar();
+        if (value == '\n' || value == EOF) break;
+
+        CharNode *cur = node_malloc(arena);
+        cur->value = value;
+        cur->next = NULL;
+
+        if (tail == NULL)
+            list->head = cur;
+        else
+            tail->next = cur;
+        cur->prev = tail;
+        tail = cur;
+    }
+}
+
+void remove_excess_spaces(CharList *list) {
+    CharNode *word = node_span(list->head, " \t");
+    list->head = word;
+
+    CharNode *space;
+    while ((space = node_anti_span(word, " \t")) != NULL) {
+        if ((word = node_span(space, " \t")) == NULL) {
+            space->prev->next = NULL;
+            break;
+        }
+        space->next = word;
+        space->value = ' ';
+    }
+}
+
+#define PRINT_NODES(prefix, nodes, suffix) \
+    printf(prefix);                        \
+    print_nodes(nodes);                    \
+    printf(suffix)
 
 int main() {
     printf("Hello world!\n");
