7d3389b5167d994fad29169b563e7c3d83a49e84
0dminnimda <0dminnimda@gmail.com>
2022-12-30 16:04:44 +0300

    Create lab6\char_list.h/.c

---
+++ lab6/char_list.c
@@ -0,0 +1,31 @@
+#include "char_list.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_nodes(CharNode *start) {
+    ITERATE_NODES(node, start) putchar(node->value);
+}
+
+CharNode *node_span(CharNode *start, char *allowed) {
+    ITERATE_NODES(node, start) {
+        bool ok = false;
+        for (char *c = allowed; *c != '\0'; c++)
+            if (node->value == *c) {
+                ok = true;
+                break;
+            }
+
+        if (!ok) return node;
+    }
+    return NULL;
+}
+
+CharNode *node_anti_span(CharNode *start, char *disallowed) {
+    ITERATE_NODES(node, start) {
+        for (char *c = disallowed; *c != '\0'; c++)
+            if (node->value == *c) return node;
+    }
+    return NULL;
+}
